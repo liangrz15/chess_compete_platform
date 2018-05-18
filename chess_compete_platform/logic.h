73d2ce68d399672cc3e851df9a19a819fb82d1eb
EverybodyHops <760953401@qq.com>
2018-05-18 16:07:36 +0800

    v2

--- chess_compete_platform/logic.h
+++ chess_compete_platform/logic.h
@@ -15,8 +15,7 @@ public:
     virtual int isVaild(int x,int y,int color);
 
     virtual Player_Color win_tie();
-    //此时x,y要为合法的落子点 传入的ui_board参数是为了让ui显示
-    //ui_board会被指向一个新的内存，原来指向的内存需要你在函数外自己维护
+    //此时x,y要为合法的落子点 传入的ui_board参数是为了维护ui显示的棋盘
     virtual void play_chess(int x,int y,Player_Color color,Board* UI_board);
     //若已经结束则true 反之false
     virtual bool isEnd();
