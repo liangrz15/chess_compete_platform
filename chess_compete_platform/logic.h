ab6e7bc6357952c1d3a5720d1570082a97338108
EverybodyHops <760953401@qq.com>
2018-05-18 15:16:26 +0800

    v2

--- chess_compete_platform/logic.h
+++ chess_compete_platform/logic.h
@@ -16,13 +16,13 @@ public:
 
     virtual Player_Color win_tie();
     //此时x,y要为合法的落子点
-    virtual void play_chess(int x,int y,Board* last_board,Player_Color color);
+    virtual void play_chess(int x,int y,Player_Color color);
     //若已经结束则true 反之false
     virtual bool isEnd();
     //保存存档
     virtual void save(QString name);
-    //读取存档
-    virtual void load(QString name);
+    //读取存档 返回下一步该谁走
+    virtual Player_Color load(QString name);
     //悔棋
     virtual bool withdraw(Player_Color color);
 };
